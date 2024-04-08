8bfd85e6408c25ea6786f841916c00d20af610d0
VoreXxx <vorex.00@mail.ru>
2024-04-08 23:19:16 +0600

    Homework done

---
+++ src/main/resources/sqlscripts/script422.sql
@@ -0,0 +1,14 @@
+CREATE TABLE cars (
+    id INTEGER PRIMARY KEY,
+    brand VARCHAR,
+    model VARCHAR,
+    cost INTEGER
+);
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR,
+    age INTEGER,
+    has_license BOOLEAN,
+    car_id INTEGER REFERENCES cars(id)
+);

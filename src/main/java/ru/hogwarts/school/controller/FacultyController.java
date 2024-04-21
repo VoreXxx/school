18d8a3e627bbc1d508f2df4af35754a46d27518a
VoreXxx <vorex.00@mail.ru>
2024-04-21 13:10:45 +0600

    First commit

--- src/main/java/ru/hogwarts/school/controller/FacultyController.java
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -1,40 +1,54 @@
 package ru.hogwarts.school.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.FacultyRepository;
 import ru.hogwarts.school.service.FacultyService;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/faculty")
 public class FacultyController {
     private final FacultyService service;
 
-    public FacultyController(FacultyService service) {
+    private final FacultyRepository facultyRepository;
+
+    public FacultyController(FacultyService service, FacultyRepository facultyRepository) {
         this.service = service;
+        this.facultyRepository = facultyRepository;
     }
 
     @GetMapping
     public Faculty get(@RequestParam long id) {
-        return service.get(id);
+        return service.get(id).getBody();
     }
 
     @PostMapping
     public Faculty add(@RequestBody Faculty faculty) {
-        return service.add(faculty);
+        return service.add(faculty).getBody();
     }
 
     @PutMapping("/{id}")
     public Faculty update(@RequestBody Faculty faculty, @PathVariable Long id) {
-        return service.update(faculty, id);
+        return service.update(faculty, id).getBody();
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable long id) {
-        service.delete(id);
+    public ResponseEntity<Faculty> delete(@PathVariable long id) {
+
+        Optional<Faculty> optionalFaculty = facultyRepository.findById(id);
+        if (optionalFaculty.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        Faculty faculty = optionalFaculty.get();
+        service.delete(faculty, id);
+        return ResponseEntity.ok(faculty);
     }
+
 
     @GetMapping("/all")
     public Collection<Faculty> getAllFaculty() {
